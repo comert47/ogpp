97aa490c64d3144a5161916dda51517ec0a7b1de
heuzef <contact@heuzef.com>
2014-04-18 09:43:51 +0200

    menu

---
+++ menu.php
@@ -0,0 +1,16 @@
+
+      <!-- menu -->
+      <div class="col-md-3">
+        <a href="#"><strong><i class="glyphicon glyphicon-briefcase"></i> Menu</strong></a>
+        <hr />
+        <ul class="nav nav-pills nav-stacked">
+          <li><a href="#"><i class="glyphicon glyphicon-user"></i> Utilisateurs</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-hdd"></i> Ordinateurs</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-hd-video"></i> Écrans</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-print"></i> Imprimantes</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-briefcase"></i> Services</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-tree-deciduous"></i> Locaux</a></li>
+        </ul>
+        <hr />
+      </div>
+      <!-- /menu -->
