43c6e31f0c936fcfb6800768b54ead8792eb18a8
heuzef <contact@heuzef.com>
2014-04-18 11:25:03 +0200

    maj ecrans

--- ecrans.php
+++ ecrans.php
@@ -7,15 +7,32 @@
 <div class="col-md-12">
   <table class="table table-striped">
     <thead>
-      <tr><th>Visits</th><th>ROI</th><th>Source</th><th>Description and Notes</th></tr>
+      <tr>
+        <th>#</th>
+        <th>Model</th>
+        <th>Taille</th>
+        <th>Affiliation</th>
+      </tr>
     </thead>
     <tbody>
-      <tr><td>45</td><td>2.45%</td><td>Direct</td><td>Sam sapien massa, aliquam in cursus ut, ullamcorper in tortor. 
-      Aliquam mauris arcu, tristique a lobortis vitae, condimentum feugiat justo.</td></tr>
-      <tr><td>289</td><td>56.2%</td><td>Referral</td><td>After RWD massa, aliquam in cursus ut, ullamcorper in tortor. 
-      Aliquam mauris arcu, tristique a lobortis vitae, condimentum feugiat justo.</td></tr>
-      <tr><td>98</td><td>25%</td><td>Type</td><td>Wil sapien massa, aliquam in cursus ut, ullamcorper in tortor. 
-      Liquam mauris arcu, tristique a lobortis vitae, condimentum feugiat justo.</td></tr>
+    <tr>
+        <td>1</td>
+        <td>Samsung</td>
+        <td>19</td>
+        <td>UC</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Samsung</td>
+        <td>19</td>
+        <td>UC</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Samsung</td>
+        <td>19</td>
+        <td>UC</td>
+    </tr>
     </tbody>
   </table>
   <hr />
