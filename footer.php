0ea984d45d5e3d37c8d2e654113fdbabfc8c03bb
heuzef <contact@heuzef.com>
2014-04-18 09:19:59 +0200

    structuration avec des includes

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+  <footer class="text-center">
+  	<p><a href="https://github.com/heuzef/ogpp">OGPP</a> : Outil de Gestion du Parc Phararon by <a href="http://heuzef.com">Heuzef</a> | License : <a href="https://github.com/heuzef/ogpp/blob/master/LICENSE">GNU GPL</a></p>
+  </footer>
+
+  <script type='text/javascript' src="./js/jquery.min.js"></script>
+  <script type='text/javascript' src="./js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+  OGPP by Heuzef (heuzef.com)
+  Powered by Bootply : http://s.bootply.com/render/85850
+  https://github.com/heuzef/ogpp
+-->
+<html lang="fr">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+  <title>OGPP</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link href="./css/bootstrap.css" rel="stylesheet">
+  <!--[if lt IE 9]>
+  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <div id="top-nav" class="navbar navbar-inverse navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-toggle"></span></button>
+        <a class="navbar-brand" href="index.php">OGPP</a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+            <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i> Root <span class="caret"></span></a>
+            <ul id="g-account-menu" class="dropdown-menu" role="menu">
+              <li><a href="#"><i class="glyphicon glyphicon-lock"></i> Déconnection</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
